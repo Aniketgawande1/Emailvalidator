4b60203a2df8c7b73f0b938762cdb004908ec4ab
Aniketgawande1 <aniketgaw2005@gamil.com>
2025-07-30 10:21:30 +0530

    test is run sucessfully

--- email.py
+++ email.py
@@ -1,16 +1,17 @@
 import re
 
 def email_validator():
-    email =input("Enter the Email: ")
     pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     
-    
-    if re.match(pattern , email):
-        print(f"{email}  is a vaild email !")
-    
-    else:
-        print(f"{email} is an email is not valid . Please try again .")
-        
-
+    while True:
+        email = input("Enter the Email (or 'quit' to exit ): ")
+        if email.lower() == 'quit':
+            print("Goodbye Buddy !")
+            break
+            
+        if re.match(pattern, email):
+            print(f"{email} is a valid email!")
+        else:
+            print(f"{email} is not a valid email. Please try again.")
 
 email_validator()
